ac2b7e3ac2f73d221ca83d88fc354fafc53d17b8
milenaf30 <milenafarotto@gmail.com>
2017-03-19 21:16:32 -0300

    Cambio el import proque me falla con el v7

--- app/src/main/java/com/tdp2/tripplanner/CitySelectionActivity.java
+++ app/src/main/java/com/tdp2/tripplanner/CitySelectionActivity.java
@@ -5,9 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.SearchView;
 
 import com.tdp2.tripplanner.citySelectionActivityExtras.CityAdapter;
 import com.tdp2.tripplanner.citySelectionActivityExtras.RecyclerItemClickListener;
